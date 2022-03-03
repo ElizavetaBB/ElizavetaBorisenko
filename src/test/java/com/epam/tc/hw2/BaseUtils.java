da1c41a7002fd8da9cdb6ca472f143650572ee95
ElizavetaBB <borisenkoli01@mail.ru>
2022-03-03 11:20:02 +0300

    change BaseUtils

--- src/test/java/com/epam/tc/hw2/BaseUtils.java
+++ src/test/java/com/epam/tc/hw2/BaseUtils.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class BaseUtils {
+public abstract class BaseUtils {
 
     protected WebDriver driver;
     protected WebDriverWait driverWait;
