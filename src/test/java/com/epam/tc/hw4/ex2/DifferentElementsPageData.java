6fe2cbcc5a1bee8a8e2d6c3f967c83d0fe5d2738
ElizavetaBB <borisenkoli01@mail.ru>
2022-03-06 16:10:27 +0300

    moved files from hw3

--- src/test/java/com/epam/tc/hw4/ex2/DifferentElementsPageData.java
+++ src/test/java/com/epam/tc/hw4/ex2/DifferentElementsPageData.java
@@ -5,7 +5,7 @@ import java.util.stream.Collectors;
 
 public class DifferentElementsPageData {
 
-    public static String PROPERTIES_PATH = "src/test/resources/hw3/test.properties";
+    public static String PROPERTIES_PATH = "src/test/resources/hw4/test.properties";
     public static String differentElementsPageTitle = "Different Elements";
     public static List<String> checkboxesNames = List.of(
             "Water", "Wind"
