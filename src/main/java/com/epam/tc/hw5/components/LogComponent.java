357390e3dac30378540e00801cee544e1122c151
ElizavetaBB <borisenkoli01@mail.ru>
2022-03-09 12:38:14 +0300

    moved files from hw4

---
+++ src/main/java/com/epam/tc/hw5/components/LogComponent.java
@@ -0,0 +1,28 @@
+package com.epam.tc.hw5.components;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class LogComponent extends AbstractBaseComponent {
+
+    @FindBy(css = ".panel-body-list.logs > li")
+    private List<WebElement> logRows;
+
+    public LogComponent(WebDriver driver, WebDriverWait wait) {
+        super(driver, wait);
+    }
+
+    public List<String> getLogRowsByMatchString(String row) {
+        return logRows
+                .stream()
+                .map(WebElement::getText)
+                .filter(elem -> elem.matches(row))
+                .collect(Collectors.toList());
+    }
+
+}
+
---
+++ src/main/java/com/epam/tc/hw5/voids/AbstractBasePage.java
@@ -0,0 +1,36 @@
+package com.epam.tc.hw5.voids;
+
+import com.epam.tc.hw5.components.HeaderComponent;
+import com.epam.tc.hw5.components.LeftMenuComponent;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public abstract class AbstractBasePage {
+
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+    protected HeaderComponent header;
+    protected LeftMenuComponent leftMenu;
+
+    protected AbstractBasePage(WebDriver driver, WebDriverWait wait) {
+        this.driver = driver;
+        this.wait = wait;
+        PageFactory.initElements(this.driver, this);
+        header = new HeaderComponent(this.driver, this.wait);
+        leftMenu = new LeftMenuComponent(this.driver, this.wait);
+    }
+
+    public HeaderComponent header() {
+        return header;
+    }
+
+    public LeftMenuComponent leftMenu() {
+        return leftMenu;
+    }
+
+    public String getTitle() {
+        return driver.getTitle();
+    }
+
+}
